f1bb15794332b991e2784720a615a7668fbac670
MonamiYP <monami.yoshioka@gmail.com>
2022-09-23 13:44:18 +0200

    Inserted title text for home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    colors: {
+      'black': '#0B0B0C',
+      'white': '#FFFFFF',
+      'pink': '#FF5761'
+    },
     extend: {},
   },
   plugins: [],
